909bed1a462f5c276d2160cb7e9e6f48e2c7b0b0
Rahul Rumalla <rahulrumalla@gmail.com>
2015-11-05 23:38:28 -0600

    Initial

---
+++ DoctorServer/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Security.AccessControl;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DoctorServer
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            List<ServerModel> servers = GetServers();
+
+            sb.AppendLine(
+                Verify("PING TEST",
+                    servers,
+                    (s) => CanPing(s),
+                    (s, b) => $"{s.Name}:\n Ping to {s.Address} was " + (b ? "OK" : "FAILED")));
+
+            sb.AppendLine(
+                Verify("TCP CONNECTION TEST",
+                    servers,
+                    (s) => CanConnectOverPort(s),
+                    (s, b) => $"{s.Name}:\n Connection to {s.Address} over port {s.Port} was " + (b ? "OK" : "FAILED")));
+
+            Console.WriteLine(sb.ToString());
+
+            Console.ReadLine();
+        }
+
+        private static string Verify(
+            string verficationTitle,
+            List<ServerModel> servers, 
+            Func<ServerModel, bool> verificationCallback,
+            Func<ServerModel, bool, string> verificationOutputCallback
+            )
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("==============================");
+            sb.AppendLine(verficationTitle);
+            sb.AppendLine("==============================");
+
+            foreach (var server in servers)
+            {
+                bool success = verificationCallback(server);
+
+                sb.AppendLine(verificationOutputCallback(server, success));
+            }
+
+            return sb.ToString();
+        }
+
+        private static List<ServerModel> GetServers()
+        {
+            var fileContents = File.ReadAllText("servers.json");
+
+            List<ServerModel> servers = JsonConvert.DeserializeObject<List<ServerModel>>(fileContents);
+
+            return servers;
+        }
+
+        private static bool CanPing(ServerModel server)
+        {
+            return CanPing(server.Address);
+        }
+
+        private static bool CanPing(string hostName)
+        {
+            bool success = false;
+
+            try
+            {
+                var ping = new Ping();
+
+                PingReply pingReply = ping.Send(hostName);
+
+                if (pingReply != null) success = pingReply.Status == IPStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return success;
+        }
+
+        private static bool CanConnectOverPort(ServerModel serverModel)
+        {
+            return CanConnectOverPort(serverModel.Address, serverModel.Port);
+        }
+
+        private static bool CanConnectOverPort(string hostName, int port)
+        {
+            bool success = false;
+
+            using (var tcpClient = new TcpClient())
+            {
+                try
+                {
+                    tcpClient.Connect(hostName, port);
+
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    tcpClient.Close();
+                }
+            }
+
+            return success;
+        }
+        private static bool HasReadPermissionOnDirectory(string path)
+        {
+            return HasPermissionOnDirectory(path, FileSystemRights.Read);
+        }
+
+        private static bool HasWritePermissionOnDirectory(string path)
+        {
+            return HasPermissionOnDirectory(path, FileSystemRights.Write);
+        }
+
+        private static bool HasPermissionOnDirectory(string path, FileSystemRights fileSystemRight)
+        {
+            bool success = false;
+
+            try
+            {
+                bool permAllow = false;
+
+                bool permDeny = false;
+
+                DirectorySecurity accessControlList = Directory.GetAccessControl(path);
+
+                if (accessControlList == null)
+                    return false;
+
+                var accessRules = accessControlList.GetAccessRules(
+                    true,
+                    true,
+                    typeof (System.Security.Principal.SecurityIdentifier)
+                    );
+
+                foreach (FileSystemAccessRule rule in accessRules)
+                {
+                    if ((fileSystemRight & rule.FileSystemRights) != fileSystemRight)
+                        continue;
+
+                    switch (rule.AccessControlType)
+                    {
+                        case AccessControlType.Allow:
+                            permAllow = true;
+                            break;
+
+                        case AccessControlType.Deny:
+                            permDeny = true;
+                            break;
+                    }
+                }
+
+                success = permAllow && !permDeny;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return success;
+        }
+    }
+}
---
+++ DoctorServer/ServerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorServer
+{
+    public class ServerModel
+    {
+        public string Name;
+        public string Address;
+        public int Port;
+    }
+}
